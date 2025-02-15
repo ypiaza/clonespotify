3e0e96e6ff0a3b0eb6072efdebf8bd2ec1cbfd3d
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-02-15 17:08:48 -0300

    Ajuste de código

--- Aula 1/Gabarito da Aula 1/src/components/SingleItem.jsx
+++ Aula 1/Gabarito da Aula 1/src/components/SingleItem.jsx
@@ -2,23 +2,11 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
-import { artistArray } from '../assets/database/artists'
 
-const Main = () => {
-  const [btnArtistHover, setBtnArtistHover] = useState(null)
-
-
+const SingleItem = () => {
+    const [btnArtistHover, setBtnArtistHover] = useState(null)
   return (
-    <div className='bg-gradient-to-b from-[#1DB954]/60 to-zinc-900 h-[100vh] rounded-2xl mx-6'>
-      <div>
-        <div className='flex items-end justify-between py-6 px-8'>
-          <h2 className='text-2xl text-white font-semibold'>Artistas populares</h2>
-          <a className='text-md text-white hover:underline' href="/">Mostrar tudo</a>
-        </div>
-
-        <div className='grid grid-cols-5 items-center justify-center'>
-          {artistArray.map((artist, index) => (
-            <div key={index} className='flex items-center justify-center hover:bg-gradient-to-b hover:from-[#1DB954]/30 hover:to-[#189243]/30 mx-4 rounded-xl p-6 duration-300 hover:shadow-2xl hover:shadow-[#1DB954]/10'
+    <div className='flex items-center justify-center hover:bg-gradient-to-b hover:from-[#1DB954]/30 hover:to-[#189243]/30 mx-4 rounded-xl p-6 duration-300 hover:shadow-2xl hover:shadow-[#1DB954]/10'
               onMouseOver={() => setBtnArtistHover(index)}
               onMouseOut={() => setBtnArtistHover(null)}>
 
@@ -35,11 +23,7 @@ const Main = () => {
                 <p className='text-white'>Artista</p>
               </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </div>
   )
 }
 
-export default Main
+export default SingleItem
