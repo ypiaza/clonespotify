c051e6ef9249bd16d2491590f0d06b999056232c
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-02-28 13:37:20 -0300

    Update da área do artista - inclusão da lista de músicas

--- Aula 1/projeto spotify/src/components/ArtistPage.jsx
+++ Aula 1/projeto spotify/src/components/ArtistPage.jsx
@@ -5,12 +5,13 @@ import { songsArray } from '../assets/database/songs';
 const ArtistPage = () => {
     const { id } = useParams();
     const artist = artistArray.find((item) => item.id === parseInt(id));
-    const songs = songsArray.filter((item) => item.id === parseInt(id));
+    const songs = songsArray.filter((item) => item.artist === artist.name);
+
 
     return (
-        <div className=' flex flex-col items-center overflow-hidden h-screen bg-gradient-to-b from-[#1DB954]/60 to-zinc-900 rounded-2xl'>
+        <div className=' flex flex-col items-center overflow-hidden h-full bg-gradient-to-b from-[#1DB954]/60 to-zinc-900 rounded-2xl'>
             {artist ? (
-                <div className='w-full h-full grid grid-cols-1 grid-rows-2'>
+                <div className='w-full h-full grid grid-cols-1'>
                     <div>
                         <div className='flex relative items-start w-full bg-center bg-cover bg-no-repeat shadow-2xl'
                             style={{
@@ -25,14 +26,33 @@ const ArtistPage = () => {
                         </div>
                     </div>
 
-                    <div className='w-full h-full '>
-                        {songs.length > 0 ? (
-                            songs.map((song) => (
-                                <h2 key={song.id} className="text-white">{console.log(song.name)}</h2>
+                    <div className='w-full'>
+                        <div className=' w-full p-4'>
+                            <h2 className='text-white text-3xl font-bold'>Populares</h2>
+                        </div>
+                        {songs ? (
+                            songs.map((item, index) => (
+                                <div className='flex items-center w-full p-4 hover:bg-white/10'>
+
+                                    <div>
+                                        <h2>{index + 1}</h2>
+                                    </div>
+                                    <div className='h-10 w-10 rounded bg-cover bg-no-repeat bg-center'
+                                         style={{
+                                            backgroundImage: `url(${item.image})`
+                                         }}
+                                    ></div>
+                                    <div className='flex text-white px-2.5'>
+
+                                        <h2 className="text-white font-semibold">{item.name}</h2>
+
+                                    </div>
+                                </div>
                             ))
                         ) : (
-                            <p>Musicas não encontradas!</p>
+                            <p className="text-gray-400">Músicas não encontradas!</p>
                         )}
+
                     </div>
                 </div>
             ) : (
