1be5ae7813a25e3fd216dddee93147b2c237b4d8
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-03-11 19:02:01 -0300

    Update

--- Aula 1/projeto spotify/src/components/PlayerSong.jsx
+++ Aula 1/projeto spotify/src/components/PlayerSong.jsx
@@ -4,14 +4,14 @@ import { songsArray } from '../assets/database/songs';
 
 const PlayerSong = () => {
     const { name } = useParams();
-    const findMusic = songsArray.find((item) => item.name === parseInt(name));
-    const music = songsArray.filter((item) => item.name === findMusic)
+    const music = songsArray.find((item) => item.name === name);
 
     return (
         <div className=' flex justify-center items-center overflow-hidden h-screen bg-gradient-to-b from-[#1DB954]/30 to-zinc-900 rounded-2xl'>
             {music ? (
-                    <div className='flex items-center justify-center h-full w-full'>
-                        <h2>{music.name}</h2>
+                    <div className='flex items-center flex-col justify-center h-full w-full'>
+                        <img src={music.image} alt="Album image" />
+                        <h2 className='text-xl text-white font-semibold'>{music.name}</h2>
                     </div>
                 
             ) :
