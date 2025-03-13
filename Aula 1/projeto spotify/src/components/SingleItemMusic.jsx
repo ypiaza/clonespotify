2b6d6959fb7dfe5c9242f75887e91475765138d1
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-03-13 18:35:39 -0300

    incluindo ação na aba de musica

--- Aula 1/projeto spotify/src/components/SingleItemMusic.jsx
+++ Aula 1/projeto spotify/src/components/SingleItemMusic.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate, useParams } from 'react-router-dom'
+import { songsArray } from '../assets/database/songs'
 
 const SingleItemMusic = (props) => {
-
-  const [btnArtistHover, setBtnArtistHover] = useState(null)
+  const navigate = useNavigate();
+  const { name } = useParams();
+  const music = songsArray.find((item) => item.name === name);
+  const [btnArtistHover, setBtnArtistHover] = useState(null);
 
   return (
     <div key={props.index} className='flex items-center justify-center hover:bg-gradient-to-b hover:from-[#1DB954]/30 hover:to-[#189243]/30 mx-4 rounded-xl p-6 duration-300 hover:shadow-2xl hover:shadow-[#1DB954]/10'
       onMouseOver={() => setBtnArtistHover(props.index)}
-      onMouseOut={() => setBtnArtistHover(null)}>
+      onMouseOut={() => setBtnArtistHover(null)}
+      onClick={() => navigate(`/playerSong/${music.artist}`)}>
 
       <div className='flex items-center justify-center flex-col'>
         <div className='overflow-hidden relative'>
